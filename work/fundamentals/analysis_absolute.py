2df663f1f3ab9ca27e29147374e5a6f27b4956d9
Mark Dunne <md24@cs1-19-11.ucc.ie>
2015-01-29 12:16:53 +0000

    work

--- work/fundamentals/analysis_absolute.py
+++ work/fundamentals/analysis_absolute.py
@@ -16,7 +16,7 @@ def to_percent(y, position):
     else:
         return s + '%'
 
-pe_returns = '/home/mark/workspace/final-year-project/data/fundamentals/pe_absolute_returns.csv'
+pe_returns = '../../data/fundamentals/pe_absolute_returns.csv'
 
 data = pd.DataFrame.from_csv(pe_returns)
 data = data[(np.abs(stats.zscore(data)) < 1).all(axis=1)]
@@ -37,12 +37,24 @@ xs = points[: , 1]
 fit = np.polyfit(xs, ys, 1)
 fit_fn = np.poly1d(fit)
 
-plt.plot(xs, ys, 'r.', xs, fit_fn(xs), 'k')
-#plt.savefig('absolute_pe_ratio_returns_unclean')
-plt.show()
-
 low_pe_ratio = data[stats.zscore(data['pe-absolute-ratio']) <= 1]
 high_pe_ratio = data[stats.zscore(data['pe-absolute-ratio']) > 1]
+
+points = low_pe_ratio.values
+ys = points[: , 0]
+xs = points[: , 1]
+plt.plot(xs, ys, 'r.', xs, fit_fn(xs), 'k')
+plt.axvline(xs.mean(), color='r', linestyle='dashed', linewidth=2)
+
+points = high_pe_ratio.values
+ys = points[: , 0]
+xs = points[: , 1]
+plt.plot(xs, ys, 'b.', xs, fit_fn(xs), 'k')
+plt.axvline(xs.mean(), color='b', linestyle='dashed', linewidth=2)
+
+plt.savefig('absolute_pe_ratio_returns_clean')
+plt.show()
+
 
 test = stats.ttest_ind(low_pe_ratio['returns'].values, high_pe_ratio['returns'].values)
 
--- work/fundamentals/authtoken.p
+++
@@ -1,3 +0,0 @@
-S'pdRRzNygCjs_YY5Y2MVe'
-p0
-.

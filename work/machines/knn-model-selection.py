2fe3513b3b1b7ad678bdef79a517c518bac90d1d
Mark Dunne <markdunne02@gmail.com>
2015-02-05 22:41:50 +0000

    knn work

---
+++ work/machines/knn-model-selection.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.cross_validation import KFold
+from sklearn.metrics import mean_squared_error
+from plot_learning_curve import plot_learning_curve
+
+def kfold_for_regression(estimator, X, y, n_folds=10, shuffle=True):
+    mses_train = np.zeros(n_folds)
+    mses_test = np.zeros(n_folds)
+    folds = KFold(n=len(X), n_folds=n_folds, shuffle=shuffle)    
+    for i, (train_indexes, test_indexes) in enumerate(folds):
+        X_train = X[train_indexes]
+        y_train = y[train_indexes]
+        X_test = X[test_indexes]
+        y_test = y[test_indexes]
+        estimator.fit(X_train, y_train)
+        y_predicted = estimator.predict(X_train)
+        mses_train[i] = mean_squared_error(y_train, y_predicted)
+        y_predicted = estimator.predict(X_test)
+        mses_test[i] = mean_squared_error(y_test, y_predicted)
+    return np.mean(mses_train), np.std(mses_train), np.mean(mses_test), np.std(mses_test)
+
+data_loc = '/home/mark/workspace/final-year-project/data/machines/DJIA-window-history.csv'
+
+print 'loading data'
+data = pd.DataFrame.from_csv(data_loc)
+
+
+print 'reducing data'
+sample = np.random.randint(len(data), size=5000)
+data = data.ix[sample]
+
+X = data[[0, 1, 2, 3, 4]].values
+y = data[['outcome']].values
+
+def bias_variance_tradeoff():
+    mses_train = np.array([])
+    std_train = np.array([])
+    mses_test = np.array([])
+    std_test = np.array([])
+    
+    neighbours = np.arange(1, 60, 2)
+    
+    for k in neighbours:
+        print 'KNeighborsRegressor(', k, ')'
+        estimator = KNeighborsRegressor(k)
+        mean_mse_train, std_mse_train, mean_mse_test, std_mse_test = kfold_for_regression(estimator, X, y, n_folds=8)
+    
+        mses_train = np.append(mses_train, mean_mse_train)
+        std_train = np.append(std_train, std_mse_train)
+    
+        mses_test = np.append(mses_test, mean_mse_test)
+        std_test = np.append(std_test, std_mse_test)
+    
+    plt.title("Bias-Variance Trade-Off for k-NN")
+    plt.xlabel("Number Neighbours")
+    plt.ylabel("MSE")
+    
+    plt.fill_between(neighbours, mses_train - std_train, mses_train + std_train, alpha=0.1, color="g")
+    plt.fill_between(neighbours, mses_test - std_test, mses_test + std_test, alpha=0.1, color="r")
+    
+    plt.plot(neighbours, mses_train, 'o-', color="g", label="Training error")                     
+    plt.plot(neighbours, mses_test, 'o-', color="r", label="Testing error")  
+    plt.legend(loc="best")
+    plt.show()                           
+
+def learning_curve():
+    estimator = KNeighborsRegressor(50)
+    folds = KFold(n=len(X), n_folds=10, shuffle=True)
+    fig = plot_learning_curve(estimator, "50-NN learning curve", X, y, cv=folds, verbose=2, train_sizes=np.linspace(.1, 1.0, 50))
+    fig.show()
+    
+if __name__ == "__main__":
+    learning_curve()    
--- work/machines/knn-test.py
+++ work/machines/knn-test.py
@@ -8,20 +8,38 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import KFold
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import classification_report
+from plot_learning_curve import plot_learning_curve
 
 data_loc = '/home/mark/workspace/final-year-project/data/machines/DJIA-window-history.csv'
 
+print 'loading data'
 data = pd.DataFrame.from_csv(data_loc)
-estimator = KNeighborsClassifier(n_neighbors=100)
+estimator = KNeighborsRegressor(n_neighbors=5)
 
-X = data[[0, 1, 2, 3, 4]].values
-y = data[['outcome-class']].values.ravel()
+print 'reducing data'
+sample = np.random.randint(len(data), size=10000)
+data = data.ix[sample]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
-
-print 'test size:', len(X_test)
-
-estimator.fit(X_train, y_train)
-y_predicted = estimator.predict(X_test)
-
-print classification_report(y_test, y_predicted)
+def test_classification():
+    X = data[[0, 1, 2, 3, 4]].values
+    y = data[['outcome-class']].values.ravel()
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+    
+    print 'test size:', len(X_test)
+    
+    estimator.fit(X_train, y_train)
+    y_predicted = estimator.predict(X_test)
+    
+    print classification_report(y_test, y_predicted)
+    
+def test_learning_curve():
+    X = data[[0, 1, 2, 3, 4]].values
+    y = data[['outcome']].values
+    folds = KFold(n=len(X), n_folds=10, shuffle=True)
+    fig = plot_learning_curve(estimator, "5 k-NN learning curve", X, y, cv=folds, verbose=2, 
+                              train_sizes=np.linspace(.1, 1.0, 20))
+    fig.show()
+    
+if __name__ == "__main__":
+    test_classification()
